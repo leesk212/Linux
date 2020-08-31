0aeecc5b8ec9b91503015ff17e3a3a2afbdafb1c
leekatme <leesk212@gmail.com>
2020-08-31 22:20:14 +0900

    Create use_getopt_fucntion.c

---
+++ SystemProgramming/1/getopt/use_getopt_fucntion.c
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////
+// Title : System Programming Assignment #1-1 ( ftp server )         //
+// Description : How to use a getopt function in the UNIX and 	     //
+///////////////////////////////////////////////////////////////////////
+#include <unistd.h>
+
+int main(int argc,char **argv)
+{
+	int aflag = 0, bflag =0;
+	char *cvalue = NULL;
+	int index,c;
+	opterr = 0;
+	
+	while  ((c = getopt (argc,argv, "abc:"))!= -1)
+	{
+		switch (c)
+		{		
+			case 'a':
+				aflag++;
+				break;
+			case 'b':
+				bflag++;
+				break;
+			case 'c':
+				cvalue = optarg;	
+			default :
+				break;	 
+		}
+
+	}
+	printf("aflag = %d, bflag = %d, cvalue = %s\n",aflag,bflag,cvalue);
+	for(index = optind; index<argc; index++)
+		printf("Non-option argument %s\n",argv[index]);
+
+	return 0;
+}
